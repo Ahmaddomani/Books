b4ce2280d4e9be3336d41e0d072f48f91a9922c4
Ahmad Aldomani <AhmadDomani2024@gmail.com>
2025-08-22 18:07:29 +0300

    add s

--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://192.168.1.111:3000",
-      "http://skinny-market123.surge.sh",
+      "https://skinny-market123.surge.sh",
     ],
     credentials: true,
   })
